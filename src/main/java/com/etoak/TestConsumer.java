b8dbd92fbbe2bd2402a7a5705ae602ccdfa04928
et1910 <et1910@etoak.com>
2020-05-04 18:00:40 +0800

    et1910用户增加注释

--- src/main/java/com/etoak/TestConsumer.java
+++ src/main/java/com/etoak/TestConsumer.java
@@ -1,7 +1,13 @@
 package com.etoak;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-
+/**
+ * et1910用户增加
+ * @author  姓名 工号
+ * @version  [版本号, 2020年5月4日]
+ * @see  [相关类/方法]
+ * @since  [产品/模块版本]
+ */
 public class TestConsumer {
     
     public static void main(String[] args) {
